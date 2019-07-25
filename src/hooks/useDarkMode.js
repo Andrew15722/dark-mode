8526ae2b6c0a651e8ff109063e60136daf2dc474
andrew-mendez <andrew15722@yahoo.com>
2019-07-25 16:18:29 -0700

    met mvp got the bug figured out.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import useLocalStorage from './useLocalStorage';
 
-const useDarkMode = (key) => {
+const useDarkMode = () => {
 	const [ isDark, setIsDark ] = useLocalStorage('true');
+	console.log(isDark);
 	useEffect(
 		() => {
-			if (isDark === 'true') {
+			// was comparing isDark to the string true istead of the boolean true
+			if (isDark === true) {
 				document.body.classList.add('dark-mode');
 			} else {
 				document.body.classList.remove('dark-mode');
